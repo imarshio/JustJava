8c216158a5e2ec81aeef35e7b7b6a75dffe51de6
marshio <2919029885@qq.com>
2022-05-09 23:58:33 +0800

    update

--- src/JavaBase/AbstractAndInterface.java
+++ src/JavaBase/AbstractAndInterface.java
@@ -28,19 +28,25 @@ public class AbstractAndInterface {
 
     public interface InterfaceTest {
         // 接口中的字段和方法默认都是public的，无需添加
-        // 接口声明常量时必须初始化，且都是static和final的
+        // 接口声明常量时必须初始化，且都是 public static final的
         static int i = 0; // Modifier 'static' is redundant for interface fields
         final int j = 0;  // Modifier 'final' is redundant for interface fields
         public int y = 0; // Modifier 'public' is redundant for interface fields
 
-        void fun1();
+        // 接口中的方法都是public abstract
+        public abstract void fun1();
 
-        default void fun2() {
+        abstract void fun2();
 
+        void fun3();
+
+        default void fun4() {
+            // default 方法 是 java8新增的，用于防止修改接口后有大量的实现类也需要跟着修改
         }
 
-        public default void fun3() {
+        public default void fun5() {
             // Modifier 'public' is redundant for interface methods
+            // 默认方法，默认是public的
         }
 
 
@@ -55,8 +61,19 @@ public class AbstractAndInterface {
     }
 
     InterfaceTest interfaceTest = new InterfaceTest() {
+
         @Override
         public void fun1() {
+
+        }
+
+        @Override
+        public void fun2() {
+
+        }
+
+        @Override
+        public void fun3() {
 
         }
     };
--- src/JavaBase/finalUsage/_02FinalVar.java
+++ src/JavaBase/finalUsage/_02FinalVar.java
@@ -14,4 +14,72 @@ public class _02FinalVar {
 
     // final 修饰的变量必须赋初始值，Variable 'j' might not have been initialized
     // private final int j;
+
+    static class TestFinal1 {
+        // 普通成员变量须在声明时赋值、或在代码块中赋值、或在构造器中赋值
+        final int b = 0;
+
+        final int a;
+        {
+            a = 2;
+        }
+
+        final int c;
+
+        public TestFinal1() {
+            c = 3;
+        }
+
+        // 类变量即static声明的变量同样需要在声明时赋值、或在静态代码块中赋值
+        final static int d = 5;
+
+        final static int e;
+        static {
+            e = 6;
+        }
+
+        // final修饰局部变量需在使用前显示初始化
+        public void test() {
+            final int f = 7;
+
+            final int g;
+
+            g = 8;
+            // 使用前赋值即可
+            System.out.println(g);
+        }
+    }
+
+    static class TestFinal2 {
+
+        // 局部变量 a、b
+        public void test(final int a) {
+            final int b = 10;
+
+            // 匿名内部类  访问局部变量
+            new Thread(() -> {
+                System.out.println(a);
+                System.out.println(b);
+            }).start();
+        }
+    }
+
+    static class TestFinal3 {
+
+        private int x = 12;
+        // 局部变量 a、b
+        public void test(final int a) {
+            final int b = 10;
+
+            // 局部内部类  访问局部变量、全局变量
+            class InnerClass {
+                public void print() {
+                    System.out.println(x);
+                    System.out.println(b);
+                }
+            }
+        }
+    }
+
+
 }
